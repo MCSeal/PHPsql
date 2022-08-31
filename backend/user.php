c5a978407dda69a10f2900d6d878c70c3c1d8e6f
MCSeal <mzeal93@gmail.com>
2022-08-31 14:41:40 -0400

    added use file

---
+++ backend/user.php
@@ -0,0 +1,21 @@
+<?php
+
+
+    class user{
+       private $db;
+
+       function __construct($conn)
+       {
+        $this->db = $conn;
+       }
+
+
+       public function insertUser(){
+
+       }
+       public function getUser(){
+
+       }
+    }
+
+?>
